7fb88fb9c468063303306a45afa55e297337322a
alumno <alumno@utnfra.com.ar>
2018-09-04 12:20:42 -0300

    swap

--- Vectores/main.c
+++ Vectores/main.c
@@ -4,35 +4,60 @@
 
 int main()
 {
-    int vector[T] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int i;
-/*
+    int j;
+    int aux;
+    int vector[T] = {80,90,50,30,2,4,5,34,56,20};
 
-    for(i = 0; i < T; i++){
+    //metodo de ordenamiento con burbujeo
 
-        printf("Ingrese un numero: \n");
-        scanf("%d", &vector[i]);
-
-
-
-*/
-
-printf("\n Todos los numeros: \n");
-
-    for(i = 0; i < T; i++){
-        printf("%d\n", vector[i]); // con & devuelve la direccion de variable
+    for(i = 0; i < T-1; i++)
+    {
+        for(j = i+1; j < T; j++)
+        {
+            if(vector[i] < vector[j]){
+                aux = vector[i];
+                vector[i] = vector[j];
+                vector[j] = aux;
+            }
+        }
     }
 
-    printf("\n Todos los numeros pares: \n");
-
     for(i = 0; i < T; i++){
+        printf("%d\n", vector[i]);
+    }
 
 
-        if(vector[i]%2==0){
-            printf("%d\n", vector[i]);
+    /*
+
+        for(i = 0; i < T; i++){
+
+            printf("Ingrese un numero: \n");
+            scanf("%d", &vector[i]);
+
+    */
+
+    /*
+
+    printf("\n Todos los numeros: \n");
+
+        for(i = 0; i < T; i++){
+            printf("%d\n", vector[i]); // con & devuelve la direccion de variable
         }
 
-    }
+        printf("\n Todos los numeros pares: \n");
+
+        for(i = 0; i < T; i++){
+
+
+            if(vector[i]%2==0){
+                printf("%d\n", vector[i]);
+            }
+
+        }
+
+    */
+
 
     return 0;
 }
